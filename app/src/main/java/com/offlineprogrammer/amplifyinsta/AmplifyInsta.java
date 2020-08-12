64f2cecff7ad6eba004147afc19f5b996a07aaa0
offlineProgrammer <mmalaka@gmail.com>
2020-08-12 16:27:12 -0700

    Initial commit

---
+++ app/src/main/java/com/offlineprogrammer/amplifyinsta/AmplifyInsta.java
@@ -0,0 +1,27 @@
+package com.offlineprogrammer.amplifyinsta;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
+
+public class AmplifyInsta extends Application {
+    private static final String TAG = "AmplifyInsta";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        try {
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.addPlugin(new AWSS3StoragePlugin());
+            Amplify.configure(getApplicationContext());
+            Log.i(TAG, "Initialized Amplify");
+        } catch (AmplifyException error) {
+            Log.e(TAG, "Could not initialize Amplify", error);
+        }
+    }
+}
+
